5f5fd07ae029cd385005f36f1974cf32779d1d69
sub722 <jhswebdeb@gmail.com>
2023-01-06 12:11:34 +0100

    added second light

--- src/components/canvas/Iplogo.js
+++ src/components/canvas/Iplogo.js
@@ -9,14 +9,17 @@ import { easing } from 'maath'
 
 export default function IPlogo(props) {
   useFrame((state, delta) => {
-    const t = (1 + Math.sin(state.clock.elapsedTime * 20)) / 2
-    stripe.current.color.setRGB(1, 2, 20 + t * 50)
-
-    light.current.intensity = 1 + t * 2
+    const t = (1 + Math.sin(state.clock.elapsedTime * 10)) / 2
+    stripe.current.color.setRGB(1 + t * 10, 2, 20 + t * 50)
+    stripe2.current.color.setRGB(1 + t * 10, 2, 20 + t * 50)
+    //light2.current.intensity = 1 + t * 2
+    //light.current.intensity = 1 + t * 2
   })
   const group = useRef()
   const stripe = useRef()
+  const stripe2 = useRef()
   const light = useRef()
+  const light2 = useRef()
   const { nodes, materials } = useGLTF('/models/iplogo2-transformed.glb')
   return (
     <group ref={group} {...props} dispose={null} rotation={[Math.PI / 2, 0, 0]} scale={5}>
@@ -25,10 +28,13 @@ export default function IPlogo(props) {
       <mesh geometry={nodes.left.geometry} material={materials['SVGMat.005']} />
       <mesh geometry={nodes.Right.geometry} material={materials['SVGMat.004']} />
       <mesh geometry={nodes.center.geometry} material={nodes.center.material} scale={[1, .8, 1]} />
-      <mesh geometry={nodes.insetBack.geometry} material={nodes.insetBack.material} position={[0, -0.0314, 0]} />
+      <mesh geometry={nodes.insetBack.geometry} material={nodes.insetBack.material} position={[0, -0.0314, 0]} >
+        <meshBasicMaterial ref={stripe2} color="blue" toneMapped={false} />
+
+      </mesh>
       <mesh geometry={nodes.insetfront.geometry} material={nodes.insetfront.material} position={[0, 0.0314, 0]} >
         <meshBasicMaterial ref={stripe} color="blue" toneMapped={false} />
-        <pointLight ref={light} intensity={1} color={[10, 2, 5]} distance={2.5} />
+
 
       </mesh>
 

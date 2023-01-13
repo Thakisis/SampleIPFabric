fb5c9337a2ae0b4f47b0f40964ddf55f6dc8bce6
Thakisis <sub722@gmail.com>
2023-01-13 13:43:45 +0100

    added proyecto

--- src/components/canvas/PreloadModels/PreloadModels.jsx
+++ src/components/canvas/PreloadModels/PreloadModels.jsx
@@ -2,6 +2,7 @@ import ComputertNetwork from './ComputerNetwork'
 import MicrosoftStudio from './MicrosoftStudio'
 import ServerRack from './ServerRack'
 import IPFabric from './IPFabric'
+import Screen from './Screen'
 import { useThree } from '@react-three/fiber'
 
 export const PreloadModels = ({ shower }) => {
@@ -12,6 +13,7 @@ export const PreloadModels = ({ shower }) => {
       <ServerRack />
       <MicrosoftStudio />
       <IPFabric position={[-2, 0, 3]} />
+      <Screen rotation={[0, Math.PI / 2, 0]} scale={.4} position={[-4, 2, -2]} />
       <mesh castShadow position={[-3, .25, .25]}
       >
         <sphereGeometry args={[0.5, 64, 64]} />
